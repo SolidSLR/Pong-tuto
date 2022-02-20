8948dcdb2941d13b452253b80c0ce88ca51b35c0
Santiago López Rey <devsantilopez@gmail.com>
2022-02-20 19:17:54 +0100

    Proxecto completado

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,19 +6,12 @@ using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.P) || Input.GetMouseButton(0)){
 
             SceneManager.LoadScene("Game");
-
         }
 
         if(Input.GetKeyDown(KeyCode.I)){
